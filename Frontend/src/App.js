90a5e66134eddc11d252cc948e14a9cd7cfbda22
Tomas Ewan Cozens <Deltax237@gmail.com>
2021-02-02 15:58:34 +0000

    Project Initialisation Done

---
+++ Frontend/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      IT IS WORKING
+    </div>
+  );
+}
+
+export default App;
--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # 35 PERI Systems Demo
 
 PERI systems project management tool demo.
+
+## Recently Updated
+Project Initialisation Done
