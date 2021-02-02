156853f3de4cfc4595d4906b6e01551a04f2ed29
Tomas Ewan Cozens <Deltax237@gmail.com>
2021-02-02 16:58:25 +0000

    Added Chakra to component

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,12 +1,27 @@
 import React from 'react'
-import ExampleComponent from '../components/ExampleComponent';
+import ExampleComponent from './components/ExampleComponent';
 import './App.css';
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
+
+// 2. Extend the theme to include custom colors, fonts, etc
+const colors = {
+  brand: {
+    primary: "#1a365d",
+    secondary: "#153e75",
+    accents: "#2a69ac",
+    open: "green",
+    closed: "red",
+  },
+}
+
+const theme = extendTheme({ colors })
+
 
 function App() {
   return (
-    <div className="App">
-      <ExampleComponent/>
-    </div>
+  <ChakraProvider theme={theme}>
+    <ExampleComponent />
+  </ChakraProvider>
   );
 }
 
---
+++ Frontend/src/components/ExampleComponent.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Box } from "@chakra-ui/react"
+
+
+const ExampleComponent = () => {
+    return (
+        <div>
+            <Box bg="brand.open" w="100%" p={4} color="white">
+                <h1>Example Component</h1>
+            </Box>
+        </div>
+    )
+}
+
+export default ExampleComponent
