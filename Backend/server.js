3b6b664f98b881e4ac7f38286c102d2a0e684f3d
Richard Mordecai <mordecairp@cardiff.ac.uk>
2021-02-03 13:38:22 +0000

    connected mongoose and express server

--- Backend/server.js
+++ Backend/server.js
@@ -26,6 +26,34 @@ app.use(router);
 
 server.listen(PORT, () => console.log(`Server has started on port ${PORT}`));
 
+const express = require('express');
+const socketio = require('socket.io');
+const http = require('http');
+
+const PORT = process.env.PORT || 5000;
+
+const router = require('./router');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+
+/**
+ * IO is here as i have read it can allow us to send
+ * emails to client with updates on the jobs
+ */
+// io.on('connection', (socket) => {
+//     console.log('');
+
+//     socket.on('disconnect', () => {
+//         console.log('');
+//     })
+// });
+
+app.use(router);
+
+server.listen(PORT, () => console.log(`Server has started on port ${PORT}`));
+
 //Mongoose
 
 //Schema Todo
