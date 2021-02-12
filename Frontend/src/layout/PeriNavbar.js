6cbe2fff02ab17f10cccf610c181c68ce4dfadf2
Tomas Ewan Cozens <Deltax237@gmail.com>
2021-02-12 20:45:29 +0000

    Navbar Logout added

--- Frontend/src/layout/PeriNavbar.js
+++ Frontend/src/layout/PeriNavbar.js
@@ -1,13 +1,15 @@
 import React, {useEffect, useState} from "react";
-import { Box, Heading } from "@chakra-ui/react";
+import { Box, Heading, Flex, Button, Spacer } from "@chakra-ui/react";
 import {ReactComponent as ReactLogo} from "../icons/Pericon.svg";
 import '.././App.css';
 import AuthService from "../services/auth.service";
+import {Link} from "react-router-dom";
 
 
 const PeriNavbar = () => {
     const [showTechnicalBoard, setShowTechnicalBoard] = useState(false);
     const [showAdminBoard, setShowAdminBoard] = useState(false);
+    const [showDesignerBoard, setShowDesignerBoard] = useState(false);
     const [currentUser, setCurrentUser] = useState(undefined);
 
     useEffect(() => {
@@ -17,6 +19,7 @@ const PeriNavbar = () => {
             setCurrentUser(user);
             setShowTechnicalBoard(user.roles.includes("ROLE_TECHNICAL"));
             setShowAdminBoard(user.roles.includes("ROLE_ADMIN"));
+            setShowDesignerBoard(user.roles.includes("ROLE_DESIGNER"));
         }
     }, []);
 
@@ -25,11 +28,35 @@ const PeriNavbar = () => {
     };
 
     return (
-    <div>
-        <Box bg="brand.background" boxShadow="lg">
-            <ReactLogo className="Logo"/>
-        </Box>
-    </div>
+    <Box bg="brand.background" boxShadow="lg">
+        <Flex alignItems="center" justifyContent="center">
+            <Box ml="15%">
+                <ReactLogo className="Logo"/>
+            </Box>
+            <Spacer/>
+            <Box mr="10%">
+            {currentUser ? (
+
+                    <Button bg="brand.secondary" color="brand.background">
+                        <a href="/Login" onClick={logOut}>
+                            LogOut
+                        </a>
+                    </Button>
+
+            ) : (
+
+                    <Button bg="brand.secondary" color="brand.background">
+                        <Link to={"/Login"}>
+                            Login
+                        </Link>
+                    </Button>
+
+            )}
+            </Box>
+
+        </Flex>
+
+    </Box>
     )
 
 };
