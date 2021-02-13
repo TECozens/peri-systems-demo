d1f8fe6253425346ea8323df64fc21c73ba1c10d
Jake Morris <jakemorrishello@gmail.com>
2021-02-13 16:19:34 +0000

    Made a `Private` component which requires authentication to route the user

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,14 +1,12 @@
 import React from 'react'
-import ExampleComponent from './components/ExampleComponent';
 import './App.css';
-import { ChakraProvider, extendTheme } from "@chakra-ui/react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import {ChakraProvider, extendTheme} from "@chakra-ui/react";
+import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
 import Login from "./components/Login"
 import PeriNavbar from "./layout/PeriNavbar";
 import Dashboard from "./components/Dashboard";
-import BoardDesigner from "./components/Dashboards/BoardDesigner";
-import BoardTechnical from "./components/Dashboards/BoardTechnical";
-import BoardAdmin from "./components/Dashboards/BoardAdmin";
+import Private from "./components/Private"
+import AuthService from "./services/auth.service";
 
 // 2. Extend the theme to include custom colors, fonts, etc
 const colors = {
@@ -27,23 +25,26 @@ const colors = {
   },
 }
 
-const theme = extendTheme({ colors })
-
+// TODO change this with new
+const USER_ROLES = ['normal', 'technical', 'admin']
+const theme = extendTheme({colors})
+const isAuthenticated = AuthService.getCurrentUser() !== null
 
 function App() {
 
+  console.log(isAuthenticated)
 
   //TODO dashboard needs to redirect unauthenticated users
   return (
-  <ChakraProvider theme={theme}>
+    <ChakraProvider theme={theme}>
       <PeriNavbar/>
       <Router>
-          <Switch>
-              <Route exact path="/Login" component={Login} />
-              <Route exact path="/dashboard" component={Dashboard} />
-          </Switch>
+        <Switch>
+          <Private exact authed={isAuthenticated} component={Dashboard} path="/dashboard"/>
+          <Route exact path="/Login" component={Login}/>
+        </Switch>
       </Router>
-  </ChakraProvider>
+    </ChakraProvider>
   );
 }
 
