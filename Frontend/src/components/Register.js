4807d1aa511f949227c7891c898a36b68c343e69
Sepehr Naseri <NaseriS@cardiff.ac.uk>
2021-02-16 14:22:31 +0000

    sign up button fix

--- Frontend/src/components/Register.js
+++ Frontend/src/components/Register.js
@@ -244,10 +244,91 @@ const Register = (props) => {
     }, []);
 
     return (
-        <div className="container">
-            <header className="jumbotron">
-                {content}
-            </header>
+        <div className="col-md-12">
+            <div className="card card-container">
+                <img
+                    src="//ssl.gstatic.com/accounts/ui/avatar_2x.png"
+                    alt="profile-img"
+                    className="profile-img-card"
+                />
+
+                <Form onSubmit={handleRegister} ref={form}>
+                    {!successful && (
+                        <div>
+
+                            <div className="form-group">
+                                <label htmlFor="firstname">Firstname</label>
+                                <Input
+                                    type="text"
+                                    className="form-control"
+                                    name="firstname"
+                                    value={firstname}
+                                    onChange={onChangeFirstname}
+                                    validations={[required, vfirstname]}
+                                />
+                            </div>
+
+                            <div className="form-group">
+                                <label htmlFor="lastname">Lastname</label>
+                                <Input
+                                    type="text"
+                                    className="form-control"
+                                    name="lastname"
+                                    value={lastname}
+                                    onChange={onChangeLastname}
+                                    validations={[required, vlastname]}
+                                />
+                            </div>
+
+                            <div className="form-group">
+                                <label htmlFor="email">Email</label>
+                                <Input
+                                    type="text"
+                                    className="form-control"
+                                    name="email"
+                                    value={email}
+                                    onChange={onChangeEmail}
+                                    validations={[required, validEmail]}
+                                />
+                            </div>
+
+                            <div className="form-group">
+                                <label htmlFor="password">Password</label>
+                                <Input
+                                    type="password"
+                                    className="form-control"
+                                    name="password"
+                                    value={password}
+                                    onChange={onChangePassword}
+                                    validations={[required, vpassword]}
+                                />
+                            </div>
+
+                            <div>
+                                <input type="checkbox" name="role" value="designer" onChange={onChangeRole}/> designer
+                                <input type="checkbox" name="role" value="technical" onChange={onChangeRole}/> technical
+                                <input type="checkbox" name="role" value="admin" onChange={onChangeRole}/> admin
+                            </div>
+
+                            <div className="form-group">
+                                <button className="btn btn-primary btn-block">Sign Up</button>
+                            </div>
+                        </div>
+                    )}
+
+                    {message && (
+                        <div className="form-group">
+                            <div
+                                className={ successful ? "alert alert-success" : "alert alert-danger" }
+                                role="alert"
+                            >
+                                {message}
+                            </div>
+                        </div>
+                    )}
+                    <CheckButton style={{ display: "none" }} ref={checkBtn} />
+                </Form>
+            </div>
         </div>
         );
 };
