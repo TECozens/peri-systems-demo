3345c5b4e15d76c5d1001af11480c1d7418f0e72
Sarah Lopes <lopessc@cardiff.ac.uk>
2021-02-25 10:57:43 +0000

    formatted BoardTechnical.js with prettier

--- Frontend/src/components/Dashboards/BoardTechnical.js
+++ Frontend/src/components/Dashboards/BoardTechnical.js
@@ -1,15 +1,5 @@
 import React, { useCallback, useEffect, useRef, useState } from "react";
-import {
-    Box,
-    Button,
-    Heading,
-    HStack,
-    Input,
-    InputGroup,
-    InputLeftElement,
-    Select,
-    Spacer,
-} from "@chakra-ui/react";
+import { Box, Button, Heading, HStack, Input, InputGroup, InputLeftElement, Select, Spacer } from "@chakra-ui/react";
 import { Table, Tbody, Td, Th, Thead, Tr } from "@chakra-ui/table";
 import { Text } from "@chakra-ui/layout";
 import ProjectService from "../../services/project.service";
@@ -68,7 +58,7 @@ const BoardTechnical = () => {
                         {
                             designersNameAndId.current[
                                 data.engineers.design_checker_id
-                            ]
+                                ]
                         }
                     </Td>
                     <Td>{data.status[data.status.length - 1].value}</Td>
