72ed84d747a36fbcfb9f20b86a03f6b6bdaf0271
Jake Morris <jakemorrishello@gmail.com>
2021-03-04 05:57:01 +0000

    Overhaul of register.js

--- Frontend/src/components/Register/Register.js
+++ Frontend/src/components/Register/Register.js
@@ -217,75 +217,157 @@
 // }
 //
 // Register.propTypes = {}
-
+import AuthService from "../../services/auth.service";
 import React, {useEffect, useState} from "react";
 import {useAsync} from 'react-async';
 import UserService from "../../services/users.service";
-import {Box, Container, Flex, Grid, GridItem} from "@chakra-ui/layout";
-import {Input, useBreakpointValue} from "@chakra-ui/react"
+import {Box, Container, Flex, Grid, GridItem, VStack} from "@chakra-ui/layout";
+import {
+    Button,
+    Input,
+    Modal,
+    ModalBody,
+    ModalContent,
+    ModalFooter,
+    ModalHeader,
+    ModalOverlay,
+    useBreakpointValue
+} from "@chakra-ui/react"
 import './Register.scss'
-import UserCount from "./UserCount/UserCount";
+import PageSection from "./UserCount/PageSection";
 import UserTable from "./UserTable/UserTable";
-import {looseIncludes} from '../../services/util.service'
 import {InputGroup, InputRightElement} from "@chakra-ui/input";
-import {AddIcon, SearchIcon} from "@chakra-ui/icons";
+import {SearchIcon} from "@chakra-ui/icons";
+import {useDisclosure} from "@chakra-ui/hooks";
+import {FormControl, FormLabel} from "@chakra-ui/form-control";
+import {Frame} from "framer"
+
+const getData = ({props}) => UserService.example(props.userSearch, props.page)
 
 const Register = props => {
-  const {data, error, isLoading} = useAsync({promiseFn: UserService.example})
-  const [userSearch, setUserSearch] = useState('')
-  const [filteredUsers, setFilteredUsers] = useState([])
-  const showUserCount = useBreakpointValue({base: false, 'md': true})
-
-  const handleUserInputChange = (event) => {
-    setUserSearch(event.target.value)
-  }
-
-  useEffect(() => {
-    console.log('happy')
-    const d = UserService.example()
-  }, [])
-
-  useEffect(() => {
-    if (data) {
-      return (userSearch !== '')
-        ? setFilteredUsers(looseIncludes(data.data, ['firstname', 'lastname', 'email'], userSearch))
-        : setFilteredUsers(data.data)
+    const [userSearch, setUserSearch] = useState('')
+    const showUserCount = useBreakpointValue({base: false, 'md': true})
+    const [page, setPage] = useState(1)
+    const [maxPage, setMaxPage] = useState(2)
+    const [searchParams, setSearchParams] = useState({page, userSearch})
+    const [onLastPage, setOnLastPage] = useState(false)
+    const [values, setValues]
+        = useState({firstname: '', lastname: '', email: ''})
+    const {data, error, isLoading}
+        = useAsync({
+        promiseFn: getData, watch: searchParams, props: {userSearch, page}
+    })
+    const [users, setUsers] = useState([])
+    const createUser = () => {
+        let user = {...values}
+        console.log(user)
+        user.roles = []
+        user.password = 'passwordDefault'
+        AuthService.register(user.firstname, user.lastname, user.email, user.password, [])
     }
-  }, [data, userSearch])
 
+    useEffect(() => {
+        console.log(data)
+        if (data) {
+            setUsers(data.data)
+        }
+    }, [data])
 
-  if (isLoading) return "Loading..."
-  if (error) return `Something went wrong: ${error.message}`
-  if (data)
+    useEffect(() => setSearchParams({page, userSearch}), [page, userSearch])
+
+    useEffect(() => setPage(1), [userSearch])
+
+    const {
+        isOpen,
+        onOpen,
+        onClose
+    } = useDisclosure()
+
+    const handleUserInputChange = (event) => {
+        setUserSearch(event.target.value)
+    }
+
+    const handleChange = ({target}) => {
+        setValues({...values, [target.name]: target.value})
+    }
+    // if (isLoading) return "Loading..."
+    // if (error) return `Something went wrong: ${error.message}`
+    // if (data)
     return (
-      <Container maxW="3xl" marginTop={12} marginBottom={12}>
-        <Flex direction='column'>
-          <Box mb={2}>
-            <Flex>
-              <InputGroup>
-              <Input autoFocus={true} value={userSearch} onChange={handleUserInputChange}
-                     placeholder='Search users by name or email'
-                     bg={'white'}/>
-                     <InputRightElement children={<SearchIcon />} />
-              </InputGroup>
+        <Container maxW="3xl" marginTop={12} marginBottom={12}>
+            <Flex direction='column'>
+
+                    <Box mb={2}>
+                        <Flex>
+                            <InputGroup>
+                                <Input autoFocus={true} value={userSearch} onChange={handleUserInputChange}
+                                       placeholder='Search users by name or email'
+                                       bg={'white'}/>
+                                <InputRightElement children={<SearchIcon/>}/>
+                            </InputGroup>
+                        </Flex>
+                    </Box>
+
+                {/*<Divider mt={4} mb={2} />*/}
+                {/*gap={4} templateColumns="repeat(7, 1fr)"*/}
+                <Grid templateColumns={showUserCount ? 'repeat(6, 1fr)' : 'repeat(5, 1fr)'} gap={4}>
+                    {showUserCount ?
+
+                            <GridItem colSpan={1} mt={6}>
+                                <VStack spacing={4}>
+                                    <Button colorScheme='yellow' onClick={onOpen}>
+                                        Create User
+                                    </Button>
+                                    <PageSection onLastPage={onLastPage} isLoading={isLoading} page={page}
+                                                 setPage={setPage}
+                                                 maxPage={maxPage}/>
+                                    <Modal isOpen={isOpen} onClose={onClose}>
+                                        <ModalOverlay/>
+                                        <ModalContent>
+                                            <ModalHeader>Edit user profile</ModalHeader>
+                                            <ModalBody p={6}>
+                                                <FormControl>
+                                                    <FormLabel>First Name</FormLabel>
+                                                    <Input value={values.firstname} onChange={handleChange}
+                                                           name='firstname'
+                                                           placeholder='First Name'/>
+                                                </FormControl>
+                                                <FormControl mt={4}>
+                                                    <FormLabel>Last name</FormLabel>
+                                                    <Input value={values.lastname} onChange={handleChange}
+                                                           name='lastname'
+                                                           placeholder="Last name"/>
+                                                </FormControl>
+                                                <FormControl mt={4}>
+                                                    <FormLabel>Email</FormLabel>
+                                                    <Input value={values.email} onChange={handleChange} name='email'
+                                                           placeholder="Email"/>
+                                                </FormControl>
+                                                {/*<FormControl mt={4}>*/}
+                                                {/*  <FormLabel>Password</FormLabel>*/}
+                                                {/*  <Input name='password' value={values.password} onChange={handleChange} placeholder="Password" />*/}
+                                                {/*</FormControl>*/}
+                                            </ModalBody>
+                                            <ModalFooter>
+                                                <Button colorScheme="yellow" mr={3} onClick={createUser}>
+                                                    Create
+                                                </Button>
+                                                <Button onClick={onClose}>Cancel</Button>
+                                            </ModalFooter>
+                                        </ModalContent>
+                                    </Modal>
+
+                                </VStack>
+                            </GridItem>
+                        :
+                        <></>
+                    }
+                        <GridItem colSpan={5}>
+                            <UserTable isLoading={isLoading} users={users}/>
+                        </GridItem>
+                </Grid>
             </Flex>
-          </Box>
-          {/*<Divider mt={4} mb={2} />*/}
-          {/*gap={4} templateColumns="repeat(7, 1fr)"*/}
-          <Grid templateColumns={showUserCount ? 'repeat(6, 1fr)' : 'repeat(5, 1fr)'}>
-            {showUserCount ?
-              <GridItem colSpan={1} >
-                <UserCount filteredUsers={filteredUsers}/>
-              </GridItem>
-              :
-              <></>
-            }
-            <GridItem colSpan={5} >
-              <UserTable filteredUsers={filteredUsers}/>
-            </GridItem>
-          </Grid>
-        </Flex>
-      </Container>
+        </Container>
     )
 }
 
