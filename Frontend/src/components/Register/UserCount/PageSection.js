c8385d435e366adf9d527a8479658565fe87df6c
Jake Morris <jakemorrishello@gmail.com>
2021-03-04 05:57:29 +0000

    PageSection now has loading states

--- Frontend/src/components/Register/UserCount/PageSection.js
+++ Frontend/src/components/Register/UserCount/PageSection.js
@@ -1,29 +1,42 @@
-import {Flex, Heading, HStack, VStack} from "@chakra-ui/layout";
+import {Box, Flex, Heading, HStack, VStack} from "@chakra-ui/layout";
 import {IconButton} from "@chakra-ui/react"
-import React, {useMemo} from "react";
+import React, {useEffect, useMemo} from "react";
 import {ChevronLeftIcon, ChevronRightIcon} from "@chakra-ui/icons";
+import {useDisclosure} from "@chakra-ui/hooks";
+import {SlideFade} from "@chakra-ui/transition";
+import {motion} from "framer-motion"
 
 const PageSection = props => {
     const nextPage = () => props.setPage(props.page + 1)
     const prevPage = () => props.setPage(props.page - 1)
 
+    const {isOpen, onOpen} = useDisclosure()
+
+    useEffect(() => {
+        onOpen()
+    })
+
     const isOnLastPage = () =>
         props.page >= props.maxPage;
 
-
     const atLastPage = useMemo(() => isOnLastPage(), [props.page])
-
 
     return (
         <VStack direction='column' w='100%' p={3} bg='#FFFFFC' alignItems='center' spacing={4} rounded={true}
                 borderRadius={6}>
+            {/*<motion.div animate={{ scale: [0, 1] }}*/}
+            {/*            transition={{ duration: 0.5 }}>*/}
+            {/*    <Box bg='red' p={10}>*/}
+            {/*        Hello*/}
+            {/*    </Box>*/}
+            {/*</motion.div>*/}
             <Flex alignItems='center' direction='column'>
                 <Heading size={'md'}>Page</Heading>
                 <Heading size={'3xl'}>{props.page}</Heading>
             </Flex>
             <HStack alignItems='center' spacing={4}>
-                <IconButton colorScheme='yellow' disabled={props.page <= 1} icon={<ChevronLeftIcon/>} onClick={prevPage}/>
-                <IconButton colorScheme='yellow' disabled={atLastPage} icon={<ChevronRightIcon/>} onClick={nextPage}/>
+                <IconButton isLoading={props.isLoading} colorScheme='yellow' disabled={props.page <= 1 || props.isLoading} icon={<ChevronLeftIcon/>} onClick={prevPage}/>
+                <IconButton isLoading={props.isLoading} colorScheme='yellow' disabled={atLastPage || props.isLoading} icon={<ChevronRightIcon/>} onClick={nextPage}/>
             </HStack>
         </VStack>
     )
