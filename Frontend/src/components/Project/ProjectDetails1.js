9e6c8f65a19aad3bad648163a644b03330caab20
Sarah Lopes <lopessc@cardiff.ac.uk>
2021-03-08 15:43:18 +0000

    created a copy of project details because unsure whether the other one can be deleted

---
+++ Frontend/src/components/Project/ProjectDetails1.js
@@ -0,0 +1,34 @@
+import React, { useEffect, useState } from "react";
+import { Box, Heading } from "@chakra-ui/layout";
+
+const ProjectDetails1 = (props) => {
+    const [project, setProject] = useState();
+
+    useEffect(() => {
+        if (props.project !== undefined) {
+            setProject(props.project);
+        }
+    }, [props.project]);
+
+    return (
+        <Box p={30}>
+            {project ? (
+                <>
+                    <Box align="center" justify="center">
+                        <Heading>{project.name}</Heading>
+                        <Heading as="h2" size="lg">
+                            (#{project.number})
+                        </Heading>
+                    </Box>
+                    <Heading as="h4" size="md" paddingLeft={20}>
+                        Description: {project.description}
+                    </Heading>
+                </>
+            ) : (
+                <> </>
+            )}
+        </Box>
+    );
+};
+
+export default ProjectDetails1;
