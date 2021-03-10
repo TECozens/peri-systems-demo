a5d9ed0d21f23c34749f53fa83e9774805387679
Sarah Lopes <lopessc@cardiff.ac.uk>
2021-03-10 09:57:06 +0000

    added contact us button that creates an email with the project name and number as subject

--- Frontend/src/components/Customer/CustomerProjectDetails.js
+++ Frontend/src/components/Customer/CustomerProjectDetails.js
@@ -1,8 +1,10 @@
 import React, { useEffect, useState } from "react";
 import { Box, Center, Container, Flex } from "@chakra-ui/layout";
+import { Button } from "@chakra-ui/react";
 import Timeline from "../Events/Timeline";
 import ProjectService from "../../services/project.service";
 import ProjectDetails1 from "../Project/ProjectDetails1";
+import { EmailIcon } from "@chakra-ui/icons";
 
 const CustomerProjectDetails = (props) => {
     const projectId = props.match.params.param1;
@@ -22,6 +24,20 @@ const CustomerProjectDetails = (props) => {
                 {projectId ? (
                     <Box m={10}>
                         <ProjectDetails1 project={project} />
+                        <Button
+                            leftIcon={<EmailIcon />}
+                            onClick={() =>
+                                (window.location.href =
+                                    "mailto:your@email.address?subject=" +
+                                    project.name +
+                                    " (#" +
+                                    project.number +
+                                    ")")
+                            }
+                            marginBottom={20}
+                        >
+                            Contact Us
+                        </Button>
                         <Timeline project={project} />
                     </Box>
                 ) : (
