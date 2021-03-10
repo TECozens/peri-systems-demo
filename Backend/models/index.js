34ea9bdbb326a180d03ff7ec66897f7c7ec77c16
Sarah Lopes <lopessc@cardiff.ac.uk>
2021-03-10 14:29:36 +0000

    removed ToDo

--- Backend/models/index.js
+++ Backend/models/index.js
@@ -1,4 +1,4 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 mongoose.Promise = global.Promise;
 
@@ -10,9 +10,6 @@ db.user = require("./user.model");
 db.role = require("./role.model");
 db.customer = require("./customerModel");
 
-//TODO Swap these out
 db.ROLES = ["designer", "technical", "admin"];
-
-
 
 module.exports = db;
