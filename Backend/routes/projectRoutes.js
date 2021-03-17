a163db7d5d6466b2198403b52afed0a8d1f40748
Sepehr Naseri <NaseriS@cardiff.ac.uk>
2021-03-17 16:08:42 +0000

    service for getting pending approval requests

--- Backend/routes/projectRoutes.js
+++ Backend/routes/projectRoutes.js
@@ -70,6 +70,26 @@ router.get(
 );
 
 router.get(
+    "/api/projects/getProjectsByDesignerEngineerWhereApprovedIsPending/:designCheckerID",
+    jsonParser,
+    (req, res) => {
+        let designCheckerID = new mongoose.Types.ObjectId(
+            req.params.designCheckerID
+        );
+        projects.find(
+            { "engineers.design_checker_id": designCheckerID, approved: "PENDING" },
+            (err, data) => {
+                if (err) {
+                    return res.json({ success: false, error: err });
+                } else {
+                    return res.json({ success: true, data: data });
+                }
+            }
+        );
+    }
+);
+
+router.get(
     "/api/projects/filter/getProjectsWithDesignEngineersByEngineerID/:engineerID/page/:page",
     jsonParser,
     (req, res) => {
