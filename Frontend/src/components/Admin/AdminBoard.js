9faf57878acf506ae30f0b6af35c5546242ec094
Sarah Lopes <LopesSC@cardiff.ac.uk>
2021-03-18 21:32:36 +0000

    removed unused imports and commented code from AdminBoard.js to remove warnings when code is run

--- Frontend/src/components/Admin/AdminBoard.js
+++ Frontend/src/components/Admin/AdminBoard.js
@@ -1,17 +1,11 @@
 import React from "react";
 import ProjectsSection from "../Project/ProjectsSection";
-import {Box, Container, Flex, Spacer} from "@chakra-ui/layout";
+import { Container } from "@chakra-ui/layout";
 
 const AdminBoard = () => {
     return (
         <Container maxW="6xl" marginTop={12} marginBottom={12}>
-                <ProjectsSection />
-{/* 
-            <Spacer m={20} />
-
-            <Box bg="brand.background" width="35%"  boxShadow="lg">
-                TODO Next Feature: Pending Requests
-            </Box> */}
+            <ProjectsSection />
         </Container>
     );
 };
