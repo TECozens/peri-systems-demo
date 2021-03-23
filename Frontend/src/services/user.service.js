6513c79da9a955bb8c250d5e212583f02995646d
Jake Morris <jakemorrishello@gmail.com>
2021-03-23 11:51:16 +0000

    Created a service method to get requests

--- Frontend/src/services/user.service.js
+++ Frontend/src/services/user.service.js
@@ -1,6 +1,7 @@
 //Data Service, users
 import axios from "axios";
 import authHeader from "./auth-header";
+import authService from "./auth.service";
 
 //TODO Remove junk file and add auth header to requests.
 const API_URL = "http://localhost:8081/api/auth/";
@@ -22,9 +23,12 @@ const getAdminBoard = () => {
     return axios.get(API_URL + "admin", { headers: authHeader() });
 };
 
+const getUserRequests = () => axios.get(`http://localhost:8081/api/users/getRequests/${authService.getCurrentUser()}`)
+
 export default {
     getPublicContent,
     getDesignerBoard,
     getTechnicalBoard,
     getAdminBoard,
+    getUserRequests
 };
