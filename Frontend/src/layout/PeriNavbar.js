27b857de1160152d00ef5944ec2d6961d3cfb293
Sarah Lopes <lopessc@cardiff.ac.uk>
2021-03-23 14:32:17 +0000

    removed unused imports from PeriNavbar.js

--- Frontend/src/layout/PeriNavbar.js
+++ Frontend/src/layout/PeriNavbar.js
@@ -1,5 +1,12 @@
 import React, { useEffect, useState } from "react";
-import { Box, Button, Container, Flex, HStack, Spacer, IconButton, useMediaQuery, Text } from "@chakra-ui/react";
+import {
+    Box,
+    Button,
+    Container,
+    HStack,
+    IconButton,
+    useMediaQuery,
+} from "@chakra-ui/react";
 import { ReactComponent as ReactLogo } from "../icons/Pericon.svg";
 import ".././App.css";
 import AuthService from "../services/auth.service";
@@ -17,7 +24,7 @@ const PeriNavbar = () => {
     const [showAdminBoard, setShowAdminBoard] = useState(false);
     const [currentUser, setCurrentUser] = useState(undefined);
     const location = useLocation();
-    const [showFullMenu] = useMediaQuery("(min-width: 800px)")
+    const [showFullMenu] = useMediaQuery("(min-width: 800px)");
 
     useEffect(() => {
         const user = AuthService.getCurrentUser();
@@ -34,64 +41,92 @@ const PeriNavbar = () => {
     };
 
     const isCurrentPage = (route) => {
-        const { pathname } = location
-        return pathname === route
-    }
+        const { pathname } = location;
+        return pathname === route;
+    };
 
     return (
         <Box bg="brand.background" boxShadow="lg">
-            <Container maxW='6xl'>
-
+            <Container maxW="6xl">
                 <HStack justifyContent="space-between">
                     <ReactLogo className="Logo" />
                     <Box mr="1em">
                         {currentUser ? (
                             <HStack>
-                                {showFullMenu ?
+                                {showFullMenu ? (
                                     <>
-                                        <Link to='Dashboard'>
-                                            <Button colorScheme='red' variant='ghost' to="/Dashboard" isActive={isCurrentPage('/Dashboard')}>
+                                        <Link to="Dashboard">
+                                            <Button
+                                                colorScheme="red"
+                                                variant="ghost"
+                                                to="/Dashboard"
+                                                isActive={isCurrentPage(
+                                                    "/Dashboard"
+                                                )}
+                                            >
                                                 Project Dashboard
                                             </Button>
                                         </Link>
-                                        {showAdminBoard ?
-                                            <Link to='Register'>
-                                                <Button colorScheme='red' variant='ghost' to="/Register" isActive={isCurrentPage('/Register')}>
+                                        {showAdminBoard ? (
+                                            <Link to="Register">
+                                                <Button
+                                                    colorScheme="red"
+                                                    variant="ghost"
+                                                    to="/Register"
+                                                    isActive={isCurrentPage(
+                                                        "/Register"
+                                                    )}
+                                                >
                                                     Manage Users
                                                 </Button>
-                                            </Link> :
+                                            </Link>
+                                        ) : (
                                             <></>
-                                        }
+                                        )}
                                     </>
-                                    : <></>
-                                }
+                                ) : (
+                                    <></>
+                                )}
 
-                                {currentUser ?
+                                {currentUser ? (
                                     <Menu isLazy>
-                                        <MenuButton as={IconButton} colorScheme='red' variant='ghost' icon={<ChevronDownIcon />}>
-
-                                        </MenuButton>
+                                        <MenuButton
+                                            as={IconButton}
+                                            colorScheme="red"
+                                            variant="ghost"
+                                            icon={<ChevronDownIcon />}
+                                        />
                                         <MenuList>
-                                            {!showFullMenu ?
-                                                <MenuGroup title='Navigation'>
-                                                    <Link to='/Dashboard'>
-                                                        <MenuItem>Project Dashboard</MenuItem>
+                                            {!showFullMenu ? (
+                                                <MenuGroup title="Navigation">
+                                                    <Link to="/Dashboard">
+                                                        <MenuItem>
+                                                            Project Dashboard
+                                                        </MenuItem>
                                                     </Link>
-                                                    {showAdminBoard ?
-                                                        <Link to='/Register'>
-                                                            <MenuItem>Manage Users</MenuItem>
-                                                        </Link> :
+                                                    {showAdminBoard ? (
+                                                        <Link to="/Register">
+                                                            <MenuItem>
+                                                                Manage Users
+                                                            </MenuItem>
+                                                        </Link>
+                                                    ) : (
                                                         <></>
-                                                    }
-                                                </MenuGroup> :
+                                                    )}
+                                                </MenuGroup>
+                                            ) : (
                                                 <></>
-                                            }
-                                            <MenuGroup title='Access'>
-                                                <MenuItem onClick={logOut}>Logout</MenuItem>
+                                            )}
+                                            <MenuGroup title="Access">
+                                                <MenuItem onClick={logOut}>
+                                                    Logout
+                                                </MenuItem>
                                             </MenuGroup>
                                         </MenuList>
                                     </Menu>
-                                    : <></>}
+                                ) : (
+                                    <></>
+                                )}
 
                                 {/* <Menu isLazy>
     <MenuButton
@@ -124,7 +159,11 @@ const PeriNavbar = () => {
                                 </Button> */}
                             </HStack>
                         ) : (
-                            <Button colorScheme='red' variant='ghost' isActive={isCurrentPage('/Login')} >
+                            <Button
+                                colorScheme="red"
+                                variant="ghost"
+                                isActive={isCurrentPage("/Login")}
+                            >
                                 <Link to="/Login">Login</Link>
                             </Button>
                         )}
